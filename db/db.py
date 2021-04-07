f068c14e66dc733432e7d565c70ebe0e7bceea3f
victor49100 <80781256+victor49100@users.noreply.github.com>
2021-04-07 10:57:58 +0200

    Delete db.py

--- db/db.py
+++
@@ -1,7 +0,0 @@
-
-import sqlite3  
-con = sqlite3.connect("list_pommes.db")  
-print("Database opened successfully")  
-con.execute("create table Student (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT NOT NULL, prix TEXT NOT NULL)")  
-print("Table created successfully")  
-con.close()   

9fe732f3fd74231143ac28fc64eb8db8ba2f63c8
victor49100 <80781256+victor49100@users.noreply.github.com>
2021-04-23 17:08:48 +0200

    Create test_class.py

---
+++ test_class.py
@@ -0,0 +1,21 @@
+## ce fichier python aurais du servir pour faire un jeux.#
+## par manque de temps et de technique nous n'avons pas eux le temps de l'implanter sur une page.#
+
+"""class pommes:
+    # self est l'atribut donnée a la classe joueur ( joueur1,joueur2 etc...)
+    def __init__(self,pv):
+        self.pv = pv
+        print ("pomme ajouter au panier")
+    def get_pv (self):
+        return print ("pomme manger;",+self.pv)
+    
+
+    def set_pv(self):
+        self.pv = self.pv - 15
+
+    def manger_pommes(self):
+        if self.pv == 10:
+            print (pomme.set_pv())
+            return ('pomme manger', self.pv)
+        else:
+            return ('...',self.pv)"""
